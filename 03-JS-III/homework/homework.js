d974192537faf0a416d7496b745f5299fe2db197
felipe benitez <cachaofpower@g,ail.com>
2022-09-19 11:55:47 -0500

    terminadatarea3

--- 03-JS-III/homework/homework.js
+++ 03-JS-III/homework/homework.js
@@ -8,12 +8,12 @@ function imprimirSumaNumeros() {
     // Imprime la suma de los números del 1 al 10
     // Pista: usa un acumulador
     // Tu código:
-    var acumulador = 0;
+    var suma = 0;
     for(var i = 0; i <= 10; i++){
       
-       acumulador = acumulador + i
+       suma = suma + i
     }
-    return acumulador;
+    return suma;
 }
 
 function encuentraPares(array){
@@ -33,25 +33,32 @@ function encuentraPares(array){
     return array2; 
   } 
    
-
-  
-
-
 function elevaAlCuadrado(array){
   // Devuelve un arreglo con cada número del array
   // elevado al cuadrado
   // Tu código:
+  
+  var cuadrado = array.map(function(elemento){return elemento ** 2});
+  
+ return cuadrado;
 }
 
 function sumaArray(array){
   // Devuelve el resultado de sumar todos los elementos
   // de un arreglo de enteros dado
   // Tu código:
+  var suma = array.reduce(function (acc, elemento){return acc + elemento});
+
+  return suma;
+
 }
 
 function numeroDigitos(num){
   // Devuelve el número de dígitos de un número dado
   // Tu código:
+  return num.toString().length;
+ 
+ 
 }
   
   
